90daab7717180f5a1c0744a2064fcba40c42ec4b
rumahkodingku <rumahkodingku45@gmail.com>
2025-09-10 15:16:01 +0700

    feat: add alert component from zard ui

--- src/app/shared/components/ui/alert/alert.variants.ts
+++ src/app/shared/components/ui/alert/alert.variants.ts
@@ -10,7 +10,8 @@ export const alertVariants = cva('relative flex gap-2 w-full rounded-lg p-4', {
       error: 'text-red-500 data-[appearance="fill"]:text-white',
     },
     zAppearance: {
-      outline: 'border data-[type="info"]:border-blue-500 data-[type="success"]:border-green-600 data-[type="warning"]:border-yellow-600 data-[type="error"]:border-red-500',
+      outline:
+        'border data-[type="info"]:border-blue-500 data-[type="success"]:border-green-600 data-[type="warning"]:border-yellow-600 data-[type="error"]:border-red-500',
       soft: 'bg-zinc-100 data-[type="info"]:bg-blue-50 data-[type="success"]:bg-green-50 data-[type="warning"]:bg-yellow-50 data-[type="error"]:bg-red-50',
       fill: 'bg-zinc-500 data-[type="info"]:bg-blue-500 data-[type="success"]:bg-green-600 data-[type="warning"]:bg-yellow-600 data-[type="error"]:bg-red-500',
     },
