a2bf35c5d00e07da248054a3e65ef0cbfbd904d2
rumahkodingku <rumahkodingku45@gmail.com>
2025-09-10 15:56:18 +0700

    refactor: update eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -32,6 +32,7 @@ export default tseslint.config(
       ],
 
       '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     },
   },
   {
