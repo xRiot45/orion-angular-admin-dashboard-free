2ce6ec50c90ccc39f9bed14b1f834ce7bc909614
rumahkodingku <rumahkodingku45@gmail.com>
2025-09-10 15:36:52 +0700

    feat: add badge component from zard ui

---
+++ src/app/shared/components/ui/badge/badge.component.ts
@@ -0,0 +1,33 @@
+import { ClassValue } from 'clsx';
+
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
+import { mergeClasses } from '@shared/utils/merge-classes';
+import { badgeVariants, ZardBadgeVariants } from './badge.variants';
+
+@Component({
+  selector: 'z-badge',
+  exportAs: 'zBadge',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+  template: `<ng-content />`,
+  host: {
+    '[class]': 'classes()',
+  },
+})
+export class ZardBadgeComponent {
+  readonly zType = input<ZardBadgeVariants['zType']>('default');
+  readonly zShape = input<ZardBadgeVariants['zShape']>('default');
+
+  readonly class = input<ClassValue>('');
+
+  protected readonly classes = computed(() =>
+    mergeClasses(badgeVariants({ zType: this.zType(), zShape: this.zShape() }), this.class()),
+  );
+}
