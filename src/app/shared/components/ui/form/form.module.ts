be9323aea9c52863d15b20f0a79f4e94d56cb5e6
rumahkodingku <rumahkodingku45@gmail.com>
2025-09-10 16:40:55 +0700

    feat: add form components from zard ui

---
+++ src/app/shared/components/ui/form/form.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+
+import {
+  ZardFormControlComponent,
+  ZardFormFieldComponent,
+  ZardFormLabelComponent,
+  ZardFormMessageComponent,
+} from './form.component';
+
+const FORM_COMPONENTS = [
+  ZardFormFieldComponent,
+  ZardFormLabelComponent,
+  ZardFormControlComponent,
+  ZardFormMessageComponent,
+];
+
+@NgModule({
+  imports: [...FORM_COMPONENTS],
+  exports: [...FORM_COMPONENTS],
+})
+export class ZardFormModule {}
