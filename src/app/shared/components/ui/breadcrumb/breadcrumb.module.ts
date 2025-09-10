dc7f90d403b3dcec320159419cb7d02ab3f5a87e
rumahkodingku <rumahkodingku45@gmail.com>
2025-09-10 15:47:24 +0700

    feat: add breadcrumb component from zard ui

---
+++ src/app/shared/components/ui/breadcrumb/breadcrumb.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import {
+  ZardBreadcrumbComponent,
+  ZardBreadcrumbEllipsisComponent,
+  ZardBreadcrumbItemComponent,
+  ZardBreadcrumbLinkComponent,
+  ZardBreadcrumbListComponent,
+  ZardBreadcrumbPageComponent,
+  ZardBreadcrumbSeparatorComponent,
+} from './breadcrumb.component';
+
+const components = [
+  ZardBreadcrumbComponent,
+  ZardBreadcrumbListComponent,
+  ZardBreadcrumbItemComponent,
+  ZardBreadcrumbLinkComponent,
+  ZardBreadcrumbPageComponent,
+  ZardBreadcrumbSeparatorComponent,
+  ZardBreadcrumbEllipsisComponent,
+];
+
+@NgModule({
+  imports: components,
+  exports: components,
+})
+export class ZardBreadcrumbModule {}
