79fddfde721eed969a30764c5db8e3aea5cf6016
rumahkodingku <rumahkodingku45@gmail.com>
2025-09-11 21:56:14 +0700

    feat: implement zard component in sidebar

--- src/app/core/layouts/default-layout/sidebar/sidebar-header/sidebar-header.component.ts
+++ src/app/core/layouts/default-layout/sidebar/sidebar-header/sidebar-header.component.ts
@@ -1,16 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA, Input } from '@angular/core';
+import { ZardAvatarComponent } from '@shared/components/ui/avatar/avatar.component';
 import { ZardDividerComponent } from '@shared/components/ui/divider/divider.component';
 import { ZardDropdownModule } from '@shared/components/ui/dropdown/dropdown.module';
 
 @Component({
   selector: 'app-sidebar-header',
-  imports: [CommonModule, ZardDropdownModule, ZardDividerComponent],
+  imports: [CommonModule, ZardDropdownModule, ZardDividerComponent, ZardAvatarComponent],
   templateUrl: './sidebar-header.component.html',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class SidebarHeaderComponent {
   @Input() isCollapsed = false;
+
+  readonly profilePicture = {
+    fallback: 'JD',
+    url: 'assets/images/avatars/avatar-1.png',
+    alt: 'User Avatar',
+  };
 
   onLogout() {
     console.log('Logout clicked');
