ddf3ec77c9177365723a0c2c48726ac280687eb9
rumahkodingku <rumahkodingku45@gmail.com>
2025-09-11 22:47:37 +0700

    refactor: refactoring code

--- src/app/core/layouts/default-layout/sidebar/sidebar-footer/sidebar-footer.component.ts
+++ src/app/core/layouts/default-layout/sidebar/sidebar-footer/sidebar-footer.component.ts
@@ -1,7 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar-footer',
+  imports: [CommonModule],
   templateUrl: './sidebar-footer.component.html',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
