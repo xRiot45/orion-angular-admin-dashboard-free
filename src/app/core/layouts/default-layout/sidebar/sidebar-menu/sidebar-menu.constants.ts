8b926928109df021c50998b0143e987940eb24f1
rumahkodingku <rumahkodingku45@gmail.com>
2025-09-12 20:20:47 +0700

    feat: add menu for sidebar

--- src/app/core/layouts/default-layout/sidebar/sidebar-menu/sidebar-menu.constants.ts
+++ src/app/core/layouts/default-layout/sidebar/sidebar-menu/sidebar-menu.constants.ts
@@ -2,7 +2,7 @@ import { NavGroup } from '@shared/types/nav.types';
 
 export const NAV_ITEMS: NavGroup[] = [
   {
-    groupTitle: 'MAIN NAVIGATION',
+    groupTitle: 'MENU',
     items: [
       {
         title: 'Dashboard',
@@ -10,36 +10,104 @@ export const NAV_ITEMS: NavGroup[] = [
         route: '/dashboard',
       },
       {
-        title: 'Profile',
-        icon: 'iconamoon:profile-circle-duotone',
+        title: 'Authentication',
+        icon: 'material-symbols:security',
         children: [
-          { title: 'Profiles' },
-          { title: 'Projects' },
-          { title: 'Works' },
-          { title: 'Teams' },
-          { title: 'Network' },
+          { title: 'Login', route: '/auth/login' },
+          { title: 'Register', route: '/auth/register' },
         ],
       },
       {
-        title: 'My Account',
-        icon: 'mdi:account-outline',
-        children: [{ title: 'General' }, { title: 'Security' }, { title: 'Notifications' }],
+        title: 'Pages',
+        icon: 'material-symbols:description',
+        children: [
+          { title: 'Error 401', route: '/error/401' },
+          { title: 'Error 404', route: '/error/404' },
+          { title: 'Error 500', route: '/error/500' },
+        ],
       },
       {
-        title: 'Authentication',
-        icon: 'material-symbols:security',
-        children: [{ title: 'Login' }, { title: 'Register' }],
+        title: 'Tables',
+        icon: 'mdi:table-large',
+        route: '/tables',
+        children: [
+          { title: 'Basic Table', route: '/tables/basic' },
+          { title: 'Data Table', route: '/tables/data' },
+        ],
       },
     ],
   },
   {
-    groupTitle: 'UTILITIES',
+    groupTitle: 'OTHERS',
     items: [
-      { title: 'About', icon: 'mdi:information-outline' },
-      { title: 'FAQ', icon: 'mdi:help-circle-outline' },
-      { title: 'Tables', icon: 'mdi:table' },
-      { title: 'Forms', icon: 'mdi:form-select' },
-      { title: 'Charts', icon: 'mdi:chart-bar' },
+      {
+        title: 'UI Elements',
+        icon: 'mdi:palette',
+        route: '/ui-elements',
+        children: [
+          // A
+          { title: 'Accordion', route: '/ui-elements/accordion' },
+          { title: 'Alert', route: '/ui-elements/alert' },
+          { title: 'Alert Dialog', route: '/ui-elements/alert-dialog' },
+          { title: 'Avatar', route: '/ui-elements/avatar' },
+
+          // B
+          { title: 'Badge', route: '/ui-elements/badge' },
+          { title: 'Breadcrumb', route: '/ui-elements/breadcrumb' },
+          { title: 'Button', route: '/ui-elements/button' },
+
+          // C
+          { title: 'Calendar', route: '/ui-elements/calendar' },
+          { title: 'Card', route: '/components/card' },
+          { title: 'Carousel', route: '/ui-elements/carousel' },
+          { title: 'Checkbox', route: '/ui-elements/checkbox' },
+          { title: 'Combobox', route: '/ui-elements/combobox' },
+          { title: 'Command', route: '/ui-elements/command' },
+
+          // D
+          { title: 'Date Picker', route: '/ui-elements/datepicker' },
+          { title: 'Dialog', route: '/ui-elements/dialog' },
+          { title: 'Divider', route: '/ui-elements/divider' },
+          { title: 'Dropdown', route: '/ui-elements/dropdown' },
+
+          // F
+          { title: 'Form', route: '/components/form' },
+
+          // I
+          { title: 'Input', route: '/ui-elements/input' },
+
+          // L
+          { title: 'Loader', route: '/ui-elements/loader' },
+
+          // M
+          { title: 'Menu', route: '/ui-elements/menu' },
+
+          // P
+          { title: 'Pagination', route: '/ui-elements/pagination' },
+          { title: 'Popover', route: '/ui-elements/popover' },
+          { title: 'Progress Bar', route: '/ui-elements/progress-bar' },
+
+          // R
+          { title: 'Radio', route: '/ui-elements/radio' },
+          { title: 'Resizable', route: '/ui-elements/resizable' },
+          { title: 'Rating', route: '/ui-elements/rating' },
+
+          // S
+          { title: 'Select', route: '/ui-elements/select' },
+          { title: 'Skeleton', route: '/ui-elements/skeleton' },
+          { title: 'Slider', route: '/ui-elements/slider' },
+          { title: 'Switch', route: '/ui-elements/switch' },
+
+          // T
+          { title: 'Tabs', route: '/ui-elements/tabs' },
+          { title: 'Toast', route: '/ui-elements/toast' },
+          { title: 'Toggle', route: '/ui-elements/toggle' },
+          { title: 'Toogle Group', route: '/ui-elements/toggle-group' },
+          { title: 'Tooltip', route: '/ui-elements/tooltip' },
+        ],
+      },
+      { title: 'Basic Charts', icon: 'mdi:chart-bar', route: '/charts/basic' },
+      { title: 'FAQ', icon: 'mdi:help-circle-outline', route: '/faq' },
     ],
   },
 ];
