d77e90288bcfe2ea109d76463d8c49f3cf8a8162
rumahkodingku <rumahkodingku45@gmail.com>
2025-09-13 11:01:12 +0700

    feat: add button component

--- src/app/modules/ui-elements/pages/button-showcase/button-showcase.component.ts
+++ src/app/modules/ui-elements/pages/button-showcase/button-showcase.component.ts
@@ -57,6 +57,84 @@ Primary Button
 `,
       description: 'This is the primary button style',
     },
+    {
+      name: 'Secondary',
+      label: 'Secondary Button',
+      classes:
+        'bg-[var(--color-secondary)] text-[var(--color-secondary-foreground)] hover:bg-[var(--color-secondary-hover)] cursor-pointer',
+      tsCode: `
+// TypeScript Code
+import { ZardButtonComponent } from 'your-path-to-button-component';
+
+@Component({
+  standalone: true,
+  imports: [ZardButtonComponent],
+  templateUrl: 'your-template.html',
+})
+export class YourComponent {}
+      `,
+      htmlCode: `
+// HTML Code
+<button z-button class="bg-[var(--color-secondary)] 
+text-[var(--color-secondary-foreground)] 
+hover:bg-[var(--color-secondary-hover)] cursor-pointer">
+Secondary Button
+</button>
+`,
+      description: 'This is the secondary button style',
+    },
+    {
+      name: 'Accent',
+      label: 'Accent Button',
+      classes:
+        'bg-[var(--color-accent)] text-[var(--color-accent-foreground)] hover:bg-[var(--color-accent-hover)] cursor-pointer',
+      tsCode: `
+// TypeScript Code
+import { ZardButtonComponent } from 'your-path-to-button-component';
+
+@Component({
+  standalone: true,
+  imports: [ZardButtonComponent],
+  templateUrl: 'your-template.html',
+})
+export class YourComponent {}
+      `,
+      htmlCode: `
+// HTML Code
+<button z-button class="bg-[var(--color-accent)] 
+text-[var(--color-accent-foreground)] 
+hover:bg-[var(--color-accent-hover)] cursor-pointer">
+Accent Button
+</button>
+`,
+      description: 'This is the accent button style',
+    },
+    {
+      name: 'Muted',
+      label: 'Muted Button',
+      classes:
+        'bg-[var(--color-muted)] text-[var(--color-muted-foreground)] hover:bg-[var(--color-muted-hover)] cursor-pointer',
+      tsCode: `
+// TypeScript Code
+import { ZardButtonComponent } from 'your-path-to-button-component';
+
+@Component({
+  standalone: true,
+  imports: [ZardButtonComponent],
+  templateUrl: 'your-template.html',
+})
+export class YourComponent {}
+      `,
+      htmlCode: `
+// HTML Code
+<button z-button class="bg-[var(--color-muted)] 
+text-[var(--color-muted-foreground)] 
+hover:bg-[var(--color-muted-hover)] cursor-pointer">
+Muted Button
+</button>
+`,
+      description: 'This is the muted button style',
+    },
   ];
 
   ngAfterViewInit(): void {
