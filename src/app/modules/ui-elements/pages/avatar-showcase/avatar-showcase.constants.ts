d35d7025b8a2546fb2f1f975e98a9c76939ce709
rumahkodingku <rumahkodingku45@gmail.com>
2025-09-15 22:32:57 +0700

    feat: add avatar with image ui elements

--- src/app/modules/ui-elements/pages/avatar-showcase/avatar-showcase.constants.ts
+++ src/app/modules/ui-elements/pages/avatar-showcase/avatar-showcase.constants.ts
@@ -6,7 +6,7 @@ interface AvatarItem {
   tsCode?: string;
   htmlCode: string;
   description?: string;
-  zImage: { fallback: string };
+  zImage: { fallback: string; url?: string; alt?: string };
 }
 
 export const AVATAR_CODE: AvatarItem[] = [
@@ -33,9 +33,43 @@ export class YourComponent {
     `,
     htmlCode: `
 <!-- HTML Code -->
-<z-avatar [zImage]="zImageDefault" />
+<z-avatar zType="default" [zImage]="zImageDefault" />
     `,
     description: 'This is the default avatar style',
-    zImage: { fallback: 'ZA' }, // ✅ definisi langsung di constants
+    zImage: { fallback: 'ZA' },
+  },
+  {
+    name: 'With Image',
+    title: 'With Image Avatar',
+    classes: '',
+    type: 'default',
+    tsCode: `
+// TypeScript Code
+import { Component } from '@angular/core';
+import { ZardAvatarComponent } from '../avatar.component';
+ 
+@Component({
+    standalone: true,
+    imports: [ZardAvatarComponent],
+    templateUrl: 'your-template.html',
+})
+export class YourComponent {
+  readonly zImageDefault = {
+    fallback: 'ZA',
+    url: '/images/avatar/imgs/avatar_image.jpg',
+    alt: 'ZadUI',
+  };
+}
+    `,
+    htmlCode: `
+<!-- HTML Code -->
+<z-avatar zType="default" [zImage]="zImageDefault" />
+    `,
+    description: 'This is the with image avatar style',
+    zImage: {
+      fallback: 'ZA',
+      url: 'https://gasevents.com/wp-content/uploads/2021/02/team-1.jpg',
+      alt: 'ZadUI',
+    },
   },
 ];
