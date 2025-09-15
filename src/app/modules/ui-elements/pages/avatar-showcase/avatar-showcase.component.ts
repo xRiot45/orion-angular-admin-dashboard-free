409a9092b1282f4ab62c5c329dbc72ec6022754b
rumahkodingku <rumahkodingku45@gmail.com>
2025-09-15 21:43:51 +0700

    feat: create component avatar showcase

---
+++ src/app/modules/ui-elements/pages/avatar-showcase/avatar-showcase.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-elements-avatar-showcase',
+  imports: [],
+  templateUrl: './avatar-showcase.component.html',
+})
+export class AvatarShowcaseComponent {}
